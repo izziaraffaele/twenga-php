0127ebff4f0839e60a54d28a37e36e90b542a456
Raffaele <izziaraffaele@gmail.com>
2016-11-13 13:41:45 +0100

    Retry Strategy

--- src/Api.php
+++ src/Api.php
@@ -6,6 +6,12 @@ class Api {
 
     protected $client;
 
+    protected $retryCode;
+
+    protected $retryMax = 0;
+    
+    protected $execution = 0;
+
     public function __construct(array $auth = null, $host = null, $version = null)
     {
         $this->client = new Client($auth, $host, $version);
@@ -22,8 +28,39 @@ class Api {
         return $this;
     }
 
+    public function retryOn($code, $times = 3)
+    {
+        $this->retryCode = (int) $code;
+        $this->retryMax = (int) $times;
+        $this->execution = 0;
+
+        return $this;
+    }
+
     public function report(array $query)
     {
-        return $this->client->send('GET', '/report', $query);
+        try
+        {
+            $response = $this->client->send('GET', '/report', $query);
+            $this->execution = 0;
+
+            return $response;
+        }
+        catch(\GuzzleHttp\Exception\RequestException $e)
+        {
+            $this->execution += 1;
+
+            if($this->shouldRetry($e->getStatusCode()))
+            {
+                return $this->report($query);
+            }
+
+            throw $e;
+        }
+    }
+
+    protected function shouldRetry($code)
+    {
+        return $this->retryCode === (int) $code && $this->retryMax > $this->execution;
     }
 }
